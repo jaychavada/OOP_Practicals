88bb6848a54dab277603ec14b91df1921b0da4a4
Jay chavada <chavdajay243@gmail.com>
2023-05-13 16:11:14 +0530

    Add files via upload

---
+++ Practical_30.java
@@ -0,0 +1,10 @@
+class Bond{
+    int a[] = {'B','O','N','D'};
+    
+}
+
+public class Practical_30 {
+    public static void main(String[] args) {
+        System.out.println("NAME : JAY CHAVADA \nENROLLMENT NO. 210210116006");
+    }
+}
